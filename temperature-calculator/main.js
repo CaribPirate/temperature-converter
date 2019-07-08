8adafac9f3a07b47ac61f78c3f1658586ea971a0
CaribPirate <52540561+CaribPirate@users.noreply.github.com>
2019-07-08 11:26:16 -0600

    Add files via upload

--- temperature-calculator/main.js
+++ temperature-calculator/main.js
@@ -1,4 +1,4 @@
-//simple addition calc
+//temperature conversion
 
 //event listener
 document.getElementById('calc-btn1').addEventListener('click', convertFahrenheit);
@@ -6,25 +6,25 @@ document.getElementById('calc-btn2').addEventListener('click', convertCelsius);
 
 //functions
 function convertFahrenheit() {
-  //input (2 numbers)
+  //input
   let fah = Number(document.getElementById('fah').value);
 
-  //process - add 2 numbers
+  //process - formula
   let conversion = (fah - 32) * 5 / 9;
 
-  //output - display sum
+  //output - display celsius
   document.getElementById('result1')
     .innerHTML = conversion;
 }
 
 function convertCelsius() {
-  //input (2 numbers)
+  //input
   let cel = Number(document.getElementById('cel').value);
 
-  //process - add 2 numbers
+  //process - formula
   let conversion = (cel * 9 / 5) + 32;
 
-  //output - display sum
+  //output - display fajernheit
   document.getElementById('result2')
     .innerHTML = conversion;
 }
